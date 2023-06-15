efdcbda759abba932be0129acc7ee648236fbb7c
jotyy <jotyy318@gmail.com>
2023-06-15 21:41:33 +0800

    feat: landing page dark mode

--- src/components/Landing/LandingContainer.tsx
+++ src/components/Landing/LandingContainer.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Flex } from '@mantine/core';
+import { Box } from '@mantine/core';
 import { ReactNode } from 'react';
 
 interface LandingContainerProps {
@@ -8,5 +8,17 @@ interface LandingContainerProps {
 }
 
 export function LandingContainer({ children }: LandingContainerProps) {
-	return <Flex direction="column">{children}</Flex>;
+	return (
+		<Box
+			sx={theme => ({
+				display: 'flex',
+				flexDirection: 'column',
+				color: theme.colorScheme === 'light' ? theme.colors.dark : theme.white,
+				background:
+					theme.colorScheme === 'light' ? theme.colors.gray[1] : theme.colors.gray[9],
+			})}
+		>
+			{children}
+		</Box>
+	);
 }
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,16 +1,23 @@
-import { Flex, Text } from '@mantine/core';
+import { Flex, Text, useMantineTheme } from '@mantine/core';
 interface Props {
 	width?: string;
 	height?: string;
 }
 
-export const Logo: React.FC<Props> = ({ width, height }) => (
-	<Flex direction="row" align="center" gap={4}>
-		<Text ff={''} fw="bolder" size="xl">
-			Mantine
-			<Text component="span" fw="normal" c="indigo">
-				Admin
+export const Logo: React.FC<Props> = ({ width, height }) => {
+	const theme = useMantineTheme();
+	return (
+		<Flex direction="row" align="center" gap={4}>
+			<Text
+				color={theme.colorScheme === 'light' ? 'dark' : 'white'}
+				fw="bolder"
+				size="xl"
+			>
+				Mantine
+				<Text component="span" fw="normal" c="indigo">
+					Admin
+				</Text>
 			</Text>
-		</Text>
-	</Flex>
-);
+		</Flex>
+	);
+};

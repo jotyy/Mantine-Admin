c3fd652e18158b04634aa9819c108b8d0df9ac16
jotyy <jotyy318@gmail.com>
2023-07-27 14:44:49 +0800

    fix: change image return function

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -6,91 +6,76 @@ export const runtime = 'edge';
 
 export const alt = 'Mantine Admin';
 export const size = {
-	width: 800,
-	height: 400,
+	width: 1200,
+	height: 630,
 };
 
 export const contentType = 'image/png';
 
-export async function GET(request: Request) {
-	try {
-		const { searchParams } = new URL(request.url);
-
-		// ?title=<title>
-		const hasTitle = searchParams.has('title');
-		const title = hasTitle
-			? searchParams.get('title')?.slice(0, 100)
-			: 'My default title';
-
-		return new ImageResponse(
-			(
+export default async function Image() {
+	return new ImageResponse(
+		(
+			<div
+				style={{
+					display: 'flex',
+					height: '100%',
+					width: '100%',
+					alignItems: 'center',
+					justifyContent: 'center',
+					letterSpacing: '-.02em',
+					fontWeight: 700,
+					background: 'white',
+				}}
+			>
+				<div
+					style={{
+						left: 42,
+						top: 42,
+						position: 'absolute',
+						display: 'flex',
+						alignItems: 'center',
+					}}
+				>
+					<span
+						style={{
+							width: 24,
+							height: 24,
+							background: 'black',
+						}}
+					/>
+					<span
+						style={{
+							marginLeft: 8,
+							fontSize: 20,
+						}}
+					>
+						@jotyy
+					</span>
+				</div>
 				<div
 					style={{
 						display: 'flex',
-						height: '100%',
-						width: '100%',
-						alignItems: 'center',
+						flexWrap: 'wrap',
 						justifyContent: 'center',
-						letterSpacing: '-.02em',
-						fontWeight: 700,
-						background: 'white',
+						padding: '20px 40px',
+						margin: '0 42px',
+						fontSize: 40,
+						width: 'auto',
+						maxWidth: 550,
+						textAlign: 'center',
+						backgroundColor: 'black',
+						color: 'white',
+						lineHeight: 1.4,
 					}}
 				>
-					<div
-						style={{
-							left: 42,
-							top: 42,
-							position: 'absolute',
-							display: 'flex',
-							alignItems: 'center',
-						}}
-					>
-						<span
-							style={{
-								width: 24,
-								height: 24,
-								background: 'black',
-							}}
-						/>
-						<span
-							style={{
-								marginLeft: 8,
-								fontSize: 20,
-							}}
-						>
-							@jotyy
-						</span>
-					</div>
-					<div
-						style={{
-							display: 'flex',
-							flexWrap: 'wrap',
-							justifyContent: 'center',
-							padding: '20px 40px',
-							margin: '0 42px',
-							fontSize: 40,
-							width: 'auto',
-							maxWidth: 550,
-							textAlign: 'center',
-							backgroundColor: 'black',
-							color: 'white',
-							lineHeight: 1.4,
-						}}
-					>
-						Mantine Admin
-						<br />
-						<p style={{ fontSize: 28, margin: 0 }}>A Modern Dashboard with Next.js.</p>
-					</div>
+					Mantine Admin
+					<br />
+					<p style={{ fontSize: 28, margin: 0 }}>A Modern Dashboard with Next.js.</p>
 				</div>
-			),
-			{
-				...size,
-			}
-		);
-	} catch (e: any) {
-		console.log(`${e.message}`);
-		return new Response(`Failed to generate the image`, {
-			status: 500,
-		});
-	}
+			</div>
+		),
+		{
+			...size,
+		}
+	);
 }

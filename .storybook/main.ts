9b6839500cde2d1ef8c916b6e98f54f953dcc4aa
jotyy <jotyy318@gmail.com>
2023-10-23 08:30:12 +0800

    feat: migrate storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,14 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 const config: StorybookConfig = {
-	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	stories: ['../src/**/*.(stories|story).@(js|jsx|ts|tsx)'],
 	addons: [
-		'@storybook/addon-links',
 		'@storybook/addon-essentials',
-		'@storybook/addon-interactions',
 		'storybook-dark-mode',
+		'@storybook/addon-styling',
 	],
 	framework: {
 		name: '@storybook/nextjs',
 		options: {},
 	},
-	docs: {
-		autodocs: 'tag',
-	},
-	staticDirs: ['../public/'],
 };
 export default config;
